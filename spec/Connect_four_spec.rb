cff441b730461d41e057406341d42d6908d81ea4
Javier Machin <javier_machin_acosta@hotmail.com>
2018-03-25 15:29:03 +0100

    Add working main loop

--- spec/Connect_four_spec.rb
+++ spec/Connect_four_spec.rb
@@ -7,28 +7,33 @@ describe ConnectFour do
     
     context "with game instance created" do
       it { is_expected.to respond_to(:current_player) }
+      it { is_expected.to respond_to(:row_counter) }
     end
 
   end
 
-  describe "input_validation" do
+  # describe "input_validation" do
 
-    context "when given 'exit' as input" do
+  #   context "when given 'exit' as input" do
    	  	
-   	  it "returns 'exit'" do
-        expect(my_game.check_input("exit")).to eql("exit")
-      end
-    end
-  end
+  #  	  it "returns 'exit'" do
+  #       expect(my_game.check_input("exit")).to eql("exit")
+  #     end
+  #   end
+  # end
 
   describe "place_token" do
 
-    context "when given '5' as location" do
+    context "when given '5' as location 2 times" do
         
-      it "'@board[5]' is a instance of Token" do
-        expect(my_game.place_token(5)).to be_instance_of(Token)
+      it "'@board[[4, 0]]' and @board[[4, 1]] are an instance of Token" do
+        my_game.place_token(5)
+        my_game.place_token(5)
+        expect(my_game.board[[4, 0]]).to be_instance_of(Token)
+        expect(my_game.board[[4, 1]]).to be_instance_of(Token)
       end
     end
+
   end
 
   describe "next_turn" do
